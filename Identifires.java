93c6de4f1a326057621249ba48c30caafb8b14a6
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-08 16:00:32 +0530

    java files

---
+++ Identifires.java
@@ -0,0 +1,11 @@
+public class Identifires {
+
+    public static void main(String[] args){
+      int String = 888;
+       System.out.println(String);
+    }
+}
+
+//IN JAVA ALLOWED CHARECTERS ARE a to z, A TO Z, 0 TO 9, $ AND _
+//JAVA IDENTIFIERS ARE CASE SENCETIVE 
+//WE CAN USE RESERVED WORDS AS IDENTIFIERS 
---
+++ ReservedWords.java
@@ -0,0 +1,6 @@
+public class ReservedWords {
+    public static void main(String[] args){
+        int WORDS = 53;
+        System.out.println(WORDS);
+    }
+}
