f1488ec84a2d392beeecac94fcf39417201d4de7
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-10 23:02:18 +0530

    java new Files

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args){
+        int x = 10;
+        System.out.println(x);
+        System.out.println("int is datatype");
+        System.out.println("x is name of the variable");
+        System.out.println("10 is the constant");
+    }
+}
+
+//THERE ARE INTEGREAL LITERALS FOR(BYTE,SHORT,INT,LONG)
+/* 1.DECIMAL LITERALS  BASE 10 ALLOWED DIGITES ARE 0-9
+   2.OCTAL FORM WITH BASE 9 ALLOWED DIGITES ARE 0-7 AND LITERAL VALUE SHOULD BE PRIFIXED WITH 0
+   3.HEXA DECIMAL FORM WITH BASE 16ALLOWED DIGITES ARE 0-9 AND A-F  AND LITERAL VALUE SHOULD PREFIXED WITH "OX" */
