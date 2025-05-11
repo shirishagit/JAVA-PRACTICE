f62dc9b866942682b3eb2fabd5196c85cc03c611
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-11 23:06:40 +0530

    Updated files

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+
+    public static void main(String[] args){ 
+        int[] x = new int[6];//one dimensional arrays
+        int[][] a = new int[2][3];// two dimensional array
+        System.out.println(x);// you will get the corresponding class names
+        System.out.println(a);//corresponding class name
+    }
+}
+
+//Every array in java is an object we can only create by using new oparator
+//you can have the size of java as 0 also
+//at every creation of Arrays wehave to mension the size of the array
---
+++ LengthOfArray.java
@@ -0,0 +1,9 @@
+public class LengthOfArray {
+    public static void main(String[] args){
+        int[][] x = new int[6][];//This is a two Dimensional Array with size 6
+        System.out.println(x.length);
+    }
+    
+}
+
+//here the length variable prints the size ogf array that is 6
