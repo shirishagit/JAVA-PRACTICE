be87fd9155d9fb2ac4f1045f17c1aaea254e3571
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-12 22:26:48 +0530

    Java Updated Files

---
+++ AssignmentOperators.java
@@ -0,0 +1,12 @@
+public class AssignmentOperators {
+    public static void main(String[] args){
+        int x =20;  // here = assigns a value to the veariable
+        int y = 10;
+        System.out.println(x+y);  //this operator adds twwo values
+         System.out.println(x-y);  //this operator subracts two values
+          System.out.println(x*y);  //this operator multiplies two values
+           System.out.println(x/y);  //this operator divides two values
+    }
+}
+
+
