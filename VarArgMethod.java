2c3658752e6c7ab335929cf9edc3cc298ce2c36b
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-18 23:10:57 +0530

    Var-arg method

---
+++ VarArgMethod.java
@@ -0,0 +1,12 @@
+public class VarArgMethod {
+    public static void m1(){
+     
+        System.out.println("VAR-ARG METHOD EXECUTION");
+    }
+    public static void main(String[] args) {
+        m1();
+    }
+}
+
+// THIS IS VAR-ARG METHOD 
+// WE CAN  DECLEARE A METHOD AND EXECUT IT AT THE TIME OOF REQUIRMENT
