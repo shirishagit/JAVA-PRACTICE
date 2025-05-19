319c288347a0d396782c5686aff946e731a4c9bd
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-19 22:43:04 +0530

    main method files

---
+++ MainMethod.java
@@ -0,0 +1,15 @@
+public class MainMethod {
+    public static void main(String[] args) {
+        System.out.println("string[]");
+    }
+    public static void main(int[] args) {
+        System.out.println("int []");
+    }
+}
+
+
+//THE ABOVE CODE IS FOR MAIN METHOD
+//WE CANNOT DECLEARE A CLASS WITHOUT MAIN METHOD
+//JVM ALWAYS SEARCHERS FOR MAIN METHOD ONLY
+//IN THE ABOVE CODE IS CALLED OVERLOADING
+//THE OUTPUT IS SRTING[]V ONLY
