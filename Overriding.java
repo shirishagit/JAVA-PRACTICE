a969e46da6909d6561c009014489822f47e21191
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-23 22:55:24 +0530

    overriding method

---
+++ Overriding.java
@@ -0,0 +1,23 @@
+ class MainOverriding {
+    public void indemandSkill(){
+        System.out.println("java");
+    }
+}
+class InnerOverriding extends MainOverriding {
+
+    public void indemandSkill(){
+        System.out.println("python");
+    }
+
+}
+
+public class Overriding{
+   
+    public static void main(String[] args) {
+        MainOverriding o = new MainOverriding();
+        o.indemandSkill();
+        InnerOverriding i = new InnerOverriding();
+        i.indemandSkill();
+    }
+    
+}
