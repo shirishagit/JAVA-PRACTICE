91c4d784b61286c11b4cdadb388f47382e4299bc
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-25 21:31:01 +0530

    interaface

---
+++ Inteface.java
@@ -0,0 +1,11 @@
+interface Inteface {
+   int x= 10;
+}
+class InnerInteface implements Inteface{
+   public static void main(String[] args) {
+     int  x=777;
+       System.out.println(x);
+   }
+    
+}
+
