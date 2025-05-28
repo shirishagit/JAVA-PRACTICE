1b865b9bf10115712b7157799b69ddde12ecc4ac
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-28 22:51:32 +0530

    DataHiding

---
+++ OOPS/DataHiding.java
@@ -0,0 +1,9 @@
+public class DataHiding {
+    private static int x = 100;
+    public static void main(String[] args) {
+        System.out.println(x);
+    }
+}
+
+
+//IN THE ABOVE CODE WEHAVE TO DECLEARE THE VARIABLE AS PRIVATE
