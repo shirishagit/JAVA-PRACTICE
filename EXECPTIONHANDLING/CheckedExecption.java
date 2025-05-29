8aede383cf9d5811eed3c8ab3ce017ebb96d1c6c
shirisha baasani <shirisha.baasani@gmail.com>
2025-05-29 22:29:07 +0530

    execption handling

---
+++ EXECPTIONHANDLING/CheckedExecption.java
@@ -0,0 +1,11 @@
+public class CheckedExecption {
+    
+    public static void main(String[] args) {
+        try {
+             System.out.println(10/0);
+        } catch (ArithmeticException e) {
+            System.out.println("error will be occured");
+        }
+       
+    }
+}
