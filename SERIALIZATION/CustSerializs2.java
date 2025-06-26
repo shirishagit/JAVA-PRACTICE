59b9c28ef6867c31d13085160e2de335c88b3b02
shirisha <shirisha.baasani@gmail.com>
2025-06-26 22:24:33 +0530

    Java Serializzation

--- SERIALIZATION/CustSerializs2.java
+++ SERIALIZATION/CustSerializs2.java
@@ -1,15 +1,19 @@
-import java.io.*;
 
-class Account implements Serializable{
+ import java.io.*;
+
+   
+
+class Account1 implements Serializable{
     String Username = "siri";
     transient int PassWord = 191130;
     private static final long serialVersionUID = 1L;
+    
 
-}
 
-public class CustomizedSerialization {
+
+public class CustSerializs2 {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Account a = new Account();
+        Account1 a = new Account1();
         System.out.println(a.Username + "..." + a.PassWord);
         FileOutputStream fos = new FileOutputStream("CustSerializable.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -18,9 +22,11 @@ public class CustomizedSerialization {
 
         FileInputStream fis = new FileInputStream("CustSerializable.txt");
         ObjectInputStream ois = new ObjectInputStream(fis);
-        Account a1 = (Account)ois.readObject();
+        Account1 a1 = (Account1)ois.readObject();
         System.out.println(a1.Username + "...." + a1.PassWord);
       
         ois.close();
     }
+
+
 }
