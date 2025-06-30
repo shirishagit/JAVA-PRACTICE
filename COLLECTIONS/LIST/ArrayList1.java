ddafa15c7dae0df2150d7bec2ff3ec5e8e09a303
shirisha <shirisha.baasani@gmail.com>
2025-06-30 19:44:34 +0530

    collections

---
+++ COLLECTIONS/LIST/ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> ar = new ArrayList<>();
+        ar.add(10);
+        ar.add(50);
+        ar.add(88);
+        ar.add(44);
+        System.out.println(ar);
+        System.out.println(ar.size());
+        System.out.println(ar.indexOf(44));
+    }
+}
