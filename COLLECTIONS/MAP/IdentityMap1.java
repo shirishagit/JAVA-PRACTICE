af888e328f431f36e0c4eca43b1c35bce083a0cd
shirisha <shirisha.baasani@gmail.com>
2025-07-02 21:38:15 +0530

    Collection

---
+++ COLLECTIONS/MAP/IdentityMap1.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+
+
+public class IdentityMap1 {
+    public static void main(String[] args) {
+        IdentityHashMap<String,Integer> h = new IdentityHashMap<>();
+        String s1 = new String("A");
+        String s2 = new String("A");
+        h.put(s1, 98);
+        h.put(s2, 97);
+        System.out.println(h);
+
+    }  
+}
---
+++ COLLECTIONS/MAP/WeakHashMap1.java
@@ -0,0 +1,7 @@
+import java.util.WeakHashMap;
+
+public class WeakHashMap1 {
+    public static void main(String[] args) {
+        WeakHashMap<String,Integer> w = new WeakHashMap<>();
+    }
+}
