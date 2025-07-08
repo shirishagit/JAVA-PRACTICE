d2bfbbc26af6001f287373429086abc840f6248c
shirisha <shirisha.baasani@gmail.com>
2025-07-08 22:11:04 +0530

    gentics

---
+++ GENETICS/Genetics1.java
@@ -0,0 +1,20 @@
+
+
+public class Genetics1 {
+    
+    
+    public static void main(String[] args) {
+         String original = "madam";
+        String s = new StringBuffer().reverse().toString();
+          if (s.equals(original)) {
+            System.out.println(original + " Is not Palindrome");
+          }else {
+            System.out.println(original + " Is Palindrome");
+          }
+
+    
+    
+   
+    
+}
+}
