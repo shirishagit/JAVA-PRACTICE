2ee636883ee2acd09477c2cd67605b5abc5d828f
shirisha <shirisha.baasani@gmail.com>
2025-07-13 20:44:20 +0530

    For Loop

---
+++ FORLOOPS/ForLoop2.java
@@ -0,0 +1,14 @@
+public class ForLoop2 {
+    public static void main(String[] args) {
+        int[]  OddNumbers = {2,4,6,8,10,12,14};
+        // for (int Num :OddNumbers){
+        //     if (Num >10) {
+        //           System.out.println(Num);
+        //     }  
+        // }
+         for(int i=0; i<OddNumbers.length;i++){
+            System.out.println(OddNumbers[i]);
+         }
+
+    }
+}
