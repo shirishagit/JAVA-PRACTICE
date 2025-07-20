0bd8f270e87ac52bbc2484e70735107cda5a5850
shirisha <shirisha.baasani@gmail.com>
2025-07-20 21:43:33 +0530

    add: arrays files

---
+++ ARRAYS/Arrays1.java
@@ -0,0 +1,13 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] arr = {0,10,20,25,29,17,7,11,99};
+         for(int i=0;i<arr.length;i++){
+            if (i%2==0) {
+                System.out.println("value of : " + i + " is " + arr[i]);
+            } else {
+                
+            }
+         }
+       
+    }
+}
