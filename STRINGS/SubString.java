9813797da0bbf2efcadb66564e72cdb6650923e5
shirisha <shirisha.baasani@gmail.com>
2025-07-27 21:52:34 +0530

    Add:String Files

---
+++ STRINGS/SubString.java
@@ -0,0 +1,11 @@
+public class SubString {
+    public static void main(String[] args) {
+        int a = 10;
+        int k = 20;
+        if (k==a) {
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+    }
+}
