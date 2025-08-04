01fe3b37d348509692f672ce2284297cae8d9394
shirisha <shirisha.baasani@gmail.com>
2025-08-04 21:22:35 +0530

    Add:String Files

--- STRINGS/SelectSorting.java
+++ STRINGS/SelectSorting.java
@@ -7,14 +7,21 @@ public class SelectSorting {
     }
      public static void selectedSort(int[] n){
            int x = n.length;
-           for (int i = 0; i < i-x; i++) {
-            for (int j = 0; j < i-x-1; j++) {
-                if (n[j]>n[j+1]) {
-                   n[j]=n[j+1];
+           for (int i = 0; i < x-1; i++) {
+            int  myIndex = i;
+            for (int j = i+1; j < x; j++) {
+                if (n[j]<n[myIndex]) {
+                    myIndex =j;
                 }
             }
+            int mainValue = n[myIndex];
+            for(int k =myIndex;k>i;k--){
+                n[k] = n[k-1];
+            }
+            n[i]= mainValue;
+        }
            }
-       }
+       
        public static void printArray(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ",");
