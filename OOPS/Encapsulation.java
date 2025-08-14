68dc38d7817eb7eaa55d50c0ca1a6ac93cd62ab8
shirisha <shirisha.baasani@gmail.com>
2025-08-14 11:15:13 +0530

    Add :String Files

--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -1,7 +1,7 @@
 public class Encapsulation {
     public static void main(String[] args) {
+        System.out.println("4 pillers of Opps concept");
         System.out.println("Encapsulation");
-        System.out.println("4 pillers");
         System.out.println("Abstraction");
         System.err.println("overriding");
     }
---
+++ STRINGS/RegularExpree.java
@@ -0,0 +1,5 @@
+public class RegularExpree {
+    public static void main(String[] args) {
+        System.out.println("Regular express ");
+    }
+}
