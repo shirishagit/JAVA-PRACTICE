e5a0362d8f558d30bb4958b1da8a689f57b12ea2
shirisha <shirisha.baasani@gmail.com>
2025-08-18 21:55:28 +0530

    Add :String Files

--- STRINGS/CountWords.java
+++ STRINGS/CountWords.java
@@ -4,6 +4,7 @@ public class CountWords {
     public static void main(String[] args) {
         String Words = "Shirisha Revathi Anusha Snehitha";
         int Count = Words.split("\\s").length;
-        System.out.println(Count);    
+        System.out.println(Count); 
+        System.out.println("Here is the count");   
     }
 }
