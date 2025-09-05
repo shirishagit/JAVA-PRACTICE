8d8b6e5131065dbd60b11265f3bd25ed666cea12
shirisha <shirisha.baasani@gmail.com>
2025-09-05 22:21:00 +0530

    Add :String Files

--- STRINGS/REgexString.java
+++ STRINGS/REgexString.java
@@ -1,7 +1,8 @@
 public class REgexString {
     public static void main(String[] args) {
-        System.out.println("Next steps");
-        System.out.println("java string");
+      String rs = "Shirisha";
+
+
         
     }
 }
