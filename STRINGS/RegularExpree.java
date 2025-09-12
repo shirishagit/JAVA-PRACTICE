d5c8996b570581f3dfea02629f32a1e38ead9dac
shirisha <shirisha.baasani@gmail.com>
2025-09-12 22:04:23 +0530

    Add :String Files

--- STRINGS/RegularExpree.java
+++ STRINGS/RegularExpree.java
@@ -1,12 +1,5 @@
 public class RegularExpree {
     public static void main(String[] args) {
-        System.out.println("Regular express ");
-        System.out.println("new formula");
-        System.out.println("shirisha");
-        System.out.println("Java jdbc");
-        System.out.println("java Spring boot application");
-        System.out.println("jdk,jvm,jre");
-        System.out.println("Crud application");
-        System.out.println("Java jdk,jvm,jre");
-    }
+        System.out.println("regular expression");
+}
 }
