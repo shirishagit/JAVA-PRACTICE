18afb81c76558498fd1c5d3b02853073f8c9a7da
shirisha <shirisha.baasani@gmail.com>
2025-09-15 08:48:32 +0530

    Add :String File

--- STRINGS/RegularExpree.java
+++ STRINGS/RegularExpree.java
@@ -1,7 +1,16 @@
 public class RegularExpree {
+
+    static int fact(int n) {
+        if (n == 0) return 1;
+        return n * fact(n - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("regular expression");
-        String st = "java";
-        System.out.println("hello "+ st);
+        int num = 5;
+        System.out.println("Factorial: " + fact(num));
+    }
 }
-}
+
+
+
+
