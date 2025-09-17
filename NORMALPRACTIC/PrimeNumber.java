e8db4c710695fda1fe8dab4b013a47086f241ac7
shirisha <shirisha.baasani@gmail.com>
2025-09-17 23:33:18 +0530

    Add :String Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -1,8 +1,10 @@
-package NORMALPRACTIC;
+
 
 public class PrimeNumber {
     public static void main(String[] args) {
         System.out.println("Prime numbers between 1 and 100 are:");
+        System.out.println("2");
+
         
     }
 }
