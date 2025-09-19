c57b8899c0d2c6fefb32ff6682059adc807be7ce
shirisha <shirisha.baasani@gmail.com>
2025-09-19 21:59:05 +0530

    Add : Practice Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -5,6 +5,7 @@ public class PrimeNumber {
         System.out.println("Prime numbers between 1 and 100 are:");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
 
         
     }
