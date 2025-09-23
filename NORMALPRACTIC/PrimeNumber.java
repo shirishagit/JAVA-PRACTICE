26a5f3c8d7236924d22bdcd10384f542c96a04e5
shirisha <shirisha.baasani@gmail.com>
2025-09-23 23:53:13 +0530

    Add String Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -7,6 +7,8 @@ public class PrimeNumber {
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
+        System.out.println("6");
+        System.out.println("s");
 
         
     }
