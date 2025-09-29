3ebe562ff0fd87af272d3a9cea8ecef6c9cf23fb
shirisha <shirisha.baasani@gmail.com>
2025-09-29 22:53:27 +0530

    Add : String Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -5,6 +5,7 @@ public class PrimeNumber {
         System.out.print("Prime numbers between 1 and 100 are: ");
         System.out.println("2,3,5,7,11,13");
         System.out.println("the prime numbers are numbers that are only divisible by 1 and itself");
+        System.out.println("the prime numbers are not divisible by any other number");
 
         
     }
