63a8f43c89014f39feb414b1238334a8b7b44fa8
shirisha <shirisha.baasani@gmail.com>
2025-10-02 21:57:06 +0530

    Add : String Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -4,21 +4,6 @@ public class PrimeNumber {
     public static void main(String[] args) {
         System.out.print("Prime numbers between 1 and 100 are: ");
        System.out.println();
-        for (int num = 1; num <= 100; num++) {
-            boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime && num > 1) {
-                System.out.print(num + " ");
-            
-        
-    }
-
-    System.out.println("hello");
 }
-    }
+    
 }
