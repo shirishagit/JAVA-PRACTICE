57ed15e5e97750ce9aa494bfecda449f7310caec
shirisha <shirisha.baasani@gmail.com>
2025-10-03 21:44:04 +0530

    Add Practic Files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -3,7 +3,8 @@
 public class PrimeNumber {
     public static void main(String[] args) {
         System.out.print("Prime numbers between 1 and 100 are: ");
-       System.out.println();
+       System.out.println("2,3,7,11,13,17,19");
+       System.out.println("here are the examples of Prime numbers");
 }
     
 }
