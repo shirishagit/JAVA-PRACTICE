101bbee96df7faa8ef72f49b5ad1c97c13dfd419
shirisha <shirisha.baasani@gmail.com>
2025-10-05 22:52:46 +0530

    Add :Practic files

--- NORMALPRACTIC/PrimeNumber.java
+++ NORMALPRACTIC/PrimeNumber.java
@@ -5,6 +5,7 @@ public class PrimeNumber {
         System.out.print("Prime numbers between 1 and 100 are: ");
        System.out.println("2,3,7,11,13,17,19");
        System.out.println("here are the examples of Prime numbers");
+       System.out.println("1. 2 is a prime number because it has only two factors: 1 and 2.");
 }
     
 }
