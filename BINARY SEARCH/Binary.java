649f8744ed042b6d2f417311ed96898b57a5b719
shirisha <shirisha.baasani@gmail.com>
2025-10-12 20:56:24 +0530

    add :Practic Files

--- BINARY SEARCH/Binary.java
+++ BINARY SEARCH/Binary.java
@@ -1,5 +1,16 @@
 public class Binary {
    public static void main(String[] args) {
     System.out.println("Hello, World!");
+    String str = "Data Structures and Algorithms";
+      System.out.println(str);
+      System.out.println(str.length());
+      System.out.println(str.charAt(5));
+      System.out.println(str.substring(5, 15));
+      System.out.println(str.indexOf("and"));
+      System.out.println(str.replace("and", "&"));
+      System.out.println(str.toLowerCase());
+      System.out.println(str.toUpperCase());
+      System.out.println(str.trim());
+
    }
 }
