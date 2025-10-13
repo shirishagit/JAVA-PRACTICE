c75c48201aaf83c7f2504eeb764704f5fd3b4ef2
shirisha <shirisha.baasani@gmail.com>
2025-10-13 21:43:51 +0530

    Add : practic files

--- SCANNER CLASS/Scanner1.java
+++ SCANNER CLASS/Scanner1.java
@@ -3,10 +3,11 @@ public class Scanner1 {
     public static void main(String[] args) throws Exception {
         Scanner c = new Scanner(System.in);
         System.out.println("Welcome");
-        String s = c.nextLine();
-        Thread.sleep(3000);
-        System.out.println("Hello " + s);
+        c.nextLine();
+
+
         c.close();
+      
 
     }
 }
